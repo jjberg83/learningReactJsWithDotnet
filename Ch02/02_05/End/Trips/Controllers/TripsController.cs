077ce6d67f0200fbee2c2c28e9bf5e7571654925
Jørund Jenssen Berg <jjberg@gmail.com>
2024-01-19 14:48:08 +0100

    Added default project files plus my own, modern version

---
+++ Ch02/02_05/End/Trips/Controllers/TripsController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Trips.Data;
+
+namespace Trips.Controllers
+{
+    [Route("api/[controller]")]
+    public class TripsController: Controller
+    {
+        private ITripService _service;
+        public TripsController(ITripService service)
+        {
+            this._service = service;
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult GetTrips()
+        {
+            var allTrips = _service.GetAllTrips();
+            return Ok(allTrips);
+        }
+
+        [HttpPost("AddTrip")]
+        public IActionResult AddTrip([FromBody]Trip trip)
+        {
+            if(trip != null) 
+            {
+                _service.AddTrip(trip);
+            }
+            return Ok();
+        }
+
+        [HttpPut("UpdateTrip/{id}")]
+        public IActionResult UpdateTrip(int id, [FromBody]Trip trip)
+        {
+            _service.UpdateTrip(id, trip);
+            return Ok(trip);
+        }
+    }
+}
---
+++ Ch03/03_07/Begin/Trips/ClientApp/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router';
+import { Layout } from './components/Layout';
+import { Home } from './components/Home';
+
+
+import './custom.css'
+import { Trips } from './components/Trip/Trips';
+import {Create} from './components/Trip/Create';
+
+export default class App extends Component {
+  static displayName = App.name;
+
+  render () {
+    return (
+      <Layout>
+        <Route exact path='/' component={Home} />
+        <Route path='/create' component = {Create} />
+        <Route path='/trips' component = {Trips} />
+      </Layout>
+    );
+  }
+}
